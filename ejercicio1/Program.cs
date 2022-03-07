4aa11bc15c61a54f0d10b77b3367a36764969fe0
Alejandro Lis <alejandro.lis@iesdoctorbalmis.es>
2022-03-07 13:40:45 +0100

    prueba

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ruta=default;
+            int i = 0;
+            Console.Write("Introduce la ruta del archivo: ");
+            ruta = Console.ReadLine();
+
+            FileStream fichero = File.Create(ruta);
+            //Encoding.UTF()
+            char[] cadena = new char[]{'h','o' ,'l', 'a', 'm', 'u', 'n', 'd', 'o' ,'c', 'r', 'u', 'e', 'l'}; 
+            byte[] cadenaByte = new byte[cadena.Length];
+            foreach(char c in cadena)
+            {
+                cadenaByte[i++] = (byte)c;
+            }
+
+            fichero.Write(cadenaByte);
+
+            fichero.Position = 0; //Lo suyo es abrir y cerrar el fichero
+            while(fichero.Position < fichero.Length)
+            {
+                Console.Write((char)fichero.ReadByte());
+            }
+            fichero.Close();
+        
+        }
+    }
+}
---
+++ ejercicio5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo cki;
+            FileStream fs = new FileStream("datos.txt", FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs, Encoding.UTF8);
+            
+            do
+            {   
+                Console.WriteLine("Tecla: ");
+                cki = Console.ReadKey();
+                bw.Write(cki.ToString());
+            }while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+            bw.Close();
+        }
+    }
+}
